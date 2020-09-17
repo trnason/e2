3cf5d6fdd2229763a7af404cc6f4e3d4a614fd57
Tommy Nason <tommy.nason@me.com>
2020-09-16 21:40:48 -0500

    Initialized P1

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: *Thomas Nason*
++ Production URL: <http://e2p1.harvardtnproject.me>
+
+## Outside resources
+n/a
+
+## Notes for instructor
+n/a
---
+++ p1/index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang='en'>
+<head><title>Project 1</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <h1>Project 1</h1>
+</body>
+</html>
