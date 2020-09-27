aad901ee01dea1956a18dbde9fdd35fbda8784c1
Tommy Nason <tommy.nason@me.com>
2020-09-26 21:30:34 -0500

    Submitting P1

---
+++ p1/index-view.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang='en'>
+<head><title>Project 1</title>
+    <meta charset='utf-8'>
+</head>
+<body>
+    <h1>Rock Paper Scissors - Game Simulator</h1>
+    <h2>Mechanics:</h2>
+
+    <ul>
+
+        <li>Player 1 randomly throws either rock, paper, or scissors.</li>
+       
+        <li>It is a tie if both player 1 and player 2 throw the same thing.</li>
+        
+        <li>Otherwise, Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.</li>
+
+    </ul>
+
+
+    <h2>Results:</h2>
+
+    <ul>
+
+        <li> Player A throws <?php echo $move1; ?> </li>
+
+    </ul>
+
+    <ul>
+
+        <li>Player B throws <?php echo $move2; ?> </li>
+
+    </ul>
+
+    <ul>
+
+        <li> <b> Player A <?php    echo $results; ?> </b> </li>
+
+    </ul>
+
+
+</body>
+</html>
--- p1/index.php
+++ p1/index.php
@@ -1,9 +1,63 @@
-<!doctype html>
-<html lang='en'>
-<head><title>Project 1</title>
-    <meta charset='utf-8'>
-</head>
-<body>
-    <h1>Project 1</h1>
-</body>
-</html>
+<?php
+
+$moves = ['rock', 'paper', 'scissors'];
+
+$randomNumber = rand(0, 2);
+
+$randomNumber1 = rand(0, 2);
+
+$move1 = $moves[$randomNumber];
+
+$move2 = $moves[$randomNumber1];
+
+$Win = 'Wins!';
+$Lose = 'Loses!';
+$Tie = 'Ties!';
+
+$results = " ";
+
+if ($move1 == $move2) 
+
+    {
+         $results = $Tie;
+    }
+
+elseif ($move1 == 'rock' and $move2 == 'paper')
+
+    {
+        $results = $Lose;   
+    }
+
+elseif ($move1 == 'scissors' and $move2 == 'paper')
+
+    {
+        $results = $Win;
+    }
+
+elseif ($move1 == 'paper' and $move2 == 'scissors')
+
+    {
+        $results = $Win;
+    }
+    
+elseif ($move1 == 'paper' and $move2 == 'rock')
+
+    {
+        $results = $Win;
+    }
+
+elseif ($move1 == 'scissors' and $move2 == 'rock')
+
+    {
+        $results = $Lose;
+    }
+
+elseif ($move1 == 'rock' and $move2 == 'scissors')
+
+    {
+        $results = $Win;
+    }
+
+
+require 'index-view.php';
+
