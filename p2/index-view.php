9862781db33be8c176a15b36c28075e7036aefc3
Tommy Nason <tommy.nason@me.com>
2020-11-03 20:40:25 -0600

    Installed new zipfoods project

--- p2/index-view.php
+++ p2/index-view.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html lang='en'>
-<head><title>Project 2</title>
+
+<head>
+    <title>Project 2</title>
     <meta charset='utf-8'>
 </head>
+
 <body>
     <h1>Rock Paper Scissors Game</h1>
     <h2>Mechanics:</h2>
@@ -12,42 +15,73 @@
         <li>You choose either rock, paper, or scissors.</li>
 
         <li> The computer will then randomly choose either rock, paper, or scissors. </li>
-       
+
         <li>It is a tie if both you and the computer choose the same thing.</li>
-        
+
         <li>Otherwise, Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.</li>
 
     </ul>
- 
-        <form method= 'GET' action='process.php'>
-        <input type = 'radio' name = 'choice' value = 'rock' id ='rock' Checked ><label for='rock'>Rock</label>
-        <input type = 'radio' name = 'choice' value = 'paper' id ='paper' ><label for='paper'>Paper</label>
-        <input type = 'radio' name = 'choice' value = 'scissors' id ='scissors' ><label for='scissors'>Scissors</label>
 
-<br>
-<br>
+    <form method='GET' action='process.php'>
+        <input type='radio' name='choice' value='rock' id='rock' Checked><label for='rock'>Rock</label>
+        <input type='radio' name='choice' value='paper' id='paper'><label for='paper'>Paper</label>
+        <input type='radio' name='choice' value='scissors' id='scissors'><label for='scissors'>Scissors</label>
 
-    <button type ='submit'>Choose</button>
+        <br>
+        <br>
+
+        <button type='submit'>Choose</button>
+
+        <li> <?php
+        
+$foo = new Foobar();
+
+var_dump(gettype($foo));
+
+
+        
+     ?>
+
+            <h2>Results</h2>
+
+            <?php if ($haveResults) { ?>
+
+            <ul>
+
+                <li> You chose: <?php echo $choice ?>
+                <li> The computer chose: <?php echo $computerchoice ?>
+                <li> Result: <?php echo $results ?>
 
 
 
-<h2>Results</h2>
 
-<?php if($haveResults) 
 
-{ ?>
 
-<ul>
- 
-    <li> You chose: <?php echo $choice ?>
-    <li> The computer chose: <?php echo $computerchoice ?>
-    <li> Result: <?php echo $results ?>
 
-</li>
 
-</ul>
 
-<?php } ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </li>
+
+            </ul>
+
+            <?php } ?>
 
 </body>
+
 </html>
--- p2/process.php
+++ p2/process.php
@@ -6,56 +6,35 @@ session_start();
 $choice = $_GET['choice'];
 
 $moves = ['rock', 'paper','scissors'];
-$computerchoice = $moves[rand(0,2)];
+$computerchoice = $moves[rand(0, 2)];
 
 $results = " ";
     
 $Win = 'You Won!';
 $Lose = 'You Lost!' ;
-$Tie = 'It is a Tie!' ; 
+$Tie = 'It is a Tie!' ;
  
 
-if ($choice == $computerchoice) 
 
-    {
-         $results = $Tie;
-    }
 
-elseif ($choice == 'rock' and $computerchoice == 'paper')
 
-    {
-        $results = $Lose;   
-    }
 
-elseif ($choice == 'scissors' and $computerchoice == 'paper')
 
-    {
-        $results = $Win;
-    }
-
-elseif ($choice == 'paper' and $computerchoice == 'scissors')
-
-    {
-        $results = $Lose;
-    }
-    
-elseif ($choice == 'paper' and $computerchoice == 'rock')
-
-    {
-        $results = $Win;
-    }
-
-elseif ($choice == 'scissors' and $computerchoice == 'rock')
-
-    {
-        $results = $Lose;
-    }
-
-elseif ($choice == 'rock' and $computerchoice == 'scissors')
-
-    {
-        $results = $Win;
-    }
+if ($choice == $computerchoice) {
+    $results = $Tie;
+} elseif ($choice == 'rock' and $computerchoice == 'paper') {
+    $results = $Lose;
+} elseif ($choice == 'scissors' and $computerchoice == 'paper') {
+    $results = $Win;
+} elseif ($choice == 'paper' and $computerchoice == 'scissors') {
+    $results = $Lose;
+} elseif ($choice == 'paper' and $computerchoice == 'rock') {
+    $results = $Win;
+} elseif ($choice == 'scissors' and $computerchoice == 'rock') {
+    $results = $Lose;
+} elseif ($choice == 'rock' and $computerchoice == 'scissors') {
+    $results = $Win;
+}
 
  
 $_SESSION['results'] = [
